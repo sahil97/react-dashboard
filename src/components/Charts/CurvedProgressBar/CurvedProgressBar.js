e7f67d825690b892655fdbb2a686e155de266d7b
sahil97 <sahil.sharma071997@gmail.com>
2019-08-26 22:01:20 +0530

    started building progress bar

---
+++ src/components/Charts/CurvedProgressBar/CurvedProgressBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CurvedProgressBar.css'
+
+const CurvedProgressBar = (props) => {
+  return (
+    <div className="curvedProgressBar">
+      <h6 className="panelTitle">Curved Progress Bar</h6>
+      <p className="subtitle">Last 10 minutes</p>
+    </div>
+  )
+}
+
+export default CurvedProgressBar;
